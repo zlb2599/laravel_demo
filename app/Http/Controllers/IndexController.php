4137b352091f4e71b4e66214cb6d407ad84934c8
zlbo <396223143@qq.com>
2017-08-07 13:57:24 +0800

    demo

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/7 0007
+ * Time: 13:52
+ */
+namespace  App\Http\Controllers;
+class IndexController extends  Controller{
+
+
+    public function index(){
+        echo 'hello word';
+    }
+}
