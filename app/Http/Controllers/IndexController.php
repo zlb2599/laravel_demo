a85eec1d85f899f51ef3e8d9923eab1ac9c7cd4a
zlbo <396223143@qq.com>
2017-08-08 10:44:57 +0800

    demo

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,10 +6,19 @@
  * Time: 13:52
  */
 namespace  App\Http\Controllers;
+use Libs;
 class IndexController extends  Controller{
 
 
     public function index(){
-        echo route('test');
+        $cl = new SphinxClient ();
+        $cl->SetServer ( 'localhost', 9312 );
+        $cl->SetConnectTimeout ( 1 );
+        $cl->SetArrayResult ( true );
+        $cl->SetMatchMode ( SPH_MATCH_ALL );
+        $cl->SetRankingMode ( SPH_RANK_PROXIMITY_BM25 );
+        $res = $cl->Query ( 1, 'mytable' );
+        var_dump($res);
+
     }
 }
